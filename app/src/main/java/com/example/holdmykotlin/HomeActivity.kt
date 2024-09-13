4142002fa62466e06eb4e20e3404717689941234
febryanalza <febryanalzaqri27@gmail.com>
2024-09-14 01:35:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/holdmykotlin/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.holdmykotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.holdmykotlin.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
---
+++ app/src/main/java/com/example/holdmykotlin/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.holdmykotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.holdmykotlin.databinding.ActivityMainBinding
+import com.example.holdmykotlin.ui.theme.HoldMyKotlinTheme
+import android.content.Intent
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.keluarMainActivity.setOnClickListener{
+            finishAffinity()
+        }
+        binding.masukMainActivity.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
